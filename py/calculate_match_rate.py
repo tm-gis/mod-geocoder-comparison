c98a649bb6d8ce4ff6640c254a3f7a81e0056ddc
Tom Lin <lint@trimet.org>
2017-03-30 13:13:34 -0700

    Created script to test match rate.

---
+++ py/calculate_match_rate.py
@@ -0,0 +1,151 @@
+from geocoder_comp_2017 import parse_mapzen_response, rlis_geocode, getAddrAndLatLng
+import pickle
+import geocoder
+import requests
+import time
+from ott.geocoder.geosolr import GeoSolr
+import random
+import datetime
+
+sample_addresses = pickle.load(open(r"G:\PUBLIC\GIS\Geocoding\geocoder_comparison\py\pickle\address_samples.p", "rb"))
+
+tm_geocoder_url = 'http://maps.trimet.org/solr'
+tm_geocoder = GeoSolr(tm_geocoder_url)
+
+# dict formatted text file of geocoders with API keys
+geocoder_file = 'P:/geocoder_dict.txt'
+
+# For Mapzen bbox parameter
+bbox = "&boundary.rect.min_lon=-123.785578" \
+        + "&boundary.rect.min_lat=44.683870" \
+        + "&boundary.rect.max_lon=-121.651006" \
+        + "&boundary.rect.max_lat=46.059685"
+
+pdxlatlng = [45.5231, -122.6765]
+
+with open(geocoder_file, 'r') as inf:
+    geocoder_dict = eval(inf.read())
+
+
+def create_test_suite(in_address):
+    # in_address = [street_number,  0
+    #               prefix,         1
+    #               street_name,    2
+    #               suffix,         3
+    #               city,           4
+    #               state,          5
+    #               zip_code]       6
+    def create_base_address():
+        current = list(in_address)
+        return " ".join(c for c in current[:4]) + ", " + current[4] + ", " + current[5]
+
+    def no_directional_prefix():
+        current = list(in_address)
+        current.pop(1)
+        return " ".join(c for c in current[:3]) + ", " + current[4]
+
+    def no_state():
+        current = list(in_address)
+        return " ".join(c for c in current[:4]) + ", " + current[4]
+
+    def wrong_suffix():
+        current = list(in_address)
+        suffix_list = ['AVE', 'ST', 'RD', 'DR', 'CT', 'BLVD', 'PL', 'BROADWAY', 'PKWY', 'TER', '', 'LN', 'HWY', 'WAY',
+                       'LOOP', 'CIR']
+        current_suffix_index = suffix_list.index(current[3])
+        while current_suffix_index == suffix_list.index(current[3]):
+            random.seed(datetime.datetime.now())
+            current_suffix_index = random.randint(0, len(suffix_list) - 1)
+        return " ".join(c for c in current[:3]) + " " + suffix_list[current_suffix_index] + ", " + current[5]
+
+    def no_commas():
+        current = list(in_address)
+        return " ".join(c for c in current)
+
+    def no_city():
+        current = list(in_address)
+        return " ".join(c for c in current[:4]) + ", " + current[5]
+
+    def misspellings():
+        current = list(in_address)
+        vowels = ['A', 'E', 'I', 'O', 'U']
+        random.seed(datetime.datetime.now())
+        random_vowel = vowels[random.randint(0, len(vowels) - 1)]
+        vowel_bool = False
+        for v in vowels:
+            if v in current[2]:
+                current_vowel = current[2].index(v)
+                vowel_bool = True
+        if vowel_bool is False:
+            current_vowel = current[2][random.randint(0, len(current[2]) - 1)]
+        while current_vowel == random_vowel:
+            random_vowel = vowels[random.randint(0, len(vowels) - 1)]
+        return " ".join(c for c in current[:2]) + " " + current[2].replace(current[2], random_vowel) + \
+               " " + current[3] + ", " + current[4] + ", " + current[5]
+
+    return [create_base_address(), no_directional_prefix(), no_state(), wrong_suffix(), no_commas(), no_city()]
+
+counter = 0
+result_dict = {}
+for address in sample_addresses:
+    counter += 1
+    if counter == 100:
+        break
+    test_suite_address_list = create_test_suite(address)
+    for test_address in test_suite_address_list:
+        base_address = " ".join(a for a in address[:4]) + ", " + address[4] + ", " + address[5]
+        anAdd = test_address
+        # print base_address, "||", anAdd
+        for aGeocoder in geocoder_dict.keys():
+            if aGeocoder not in result_dict:
+                result_dict[aGeocoder] = {}
+            if base_address not in result_dict[aGeocoder]:
+                result_dict[aGeocoder][base_address] = []
+            if aGeocoder == 'trimet':
+                gc = tm_geocoder.query(anAdd, rows=1, start=0)
+                try:
+                    results = gc.results[0]
+                    addr = (str.decode('{0}, {1}, OR'.format(results['name'].replace("'", "\''"), results['city'])))
+                except:
+                    addr = 'Null'
+                    lat = 'Null'
+                    lng = 'Null'
+               # I have to call mapzen outside of the geocoder wrapper because
+                # without the bbox parameters (which I can't pass through geocoder)
+                # it was returning some very wacky results - Kansas, Iowa, FL -
+                # and just in the first 10 records.
+            elif aGeocoder == 'mapzen':
+                request = "https://search.mapzen.com/v1/search?api_key=" \
+                        + geocoder_dict[aGeocoder] + "&text=" + anAdd + bbox
+                resp = requests.get(request, verify=False).text
+                result = parse_mapzen_response(resp)
+                addr = result['label']
+                lat = result['latitude']
+                lng = result['longitude']
+            elif aGeocoder == 'rlis':
+                result = rlis_geocode(anAdd, geocoder_dict[aGeocoder])
+                if result:
+                    addr = result['fullAddress']
+                    lat = result['lat']
+                    lng = result['lng']
+                else:
+                    addr = 'Null'
+                    lat = 'Null'
+                    lng = 'Null'
+            else:
+                this_geocoder = getattr(geocoder, aGeocoder)
+                if geocoder_dict[aGeocoder] == '':
+                    g = this_geocoder(anAdd)
+                    while g.status == 'OVER_QUERY_LIMIT':
+                        print ("over", aGeocoder,
+                               "query limit - will try again in 30 minutes")
+                        time.sleep(1800)
+                        g = this_geocoder(anAdd)
+                else:
+                    # another possible parameter to pass for mapbox is
+                    # proximity=pdxlatlng (makes it favor results near that
+                    # point) but it didn't improve things for a small sample
+                    g = this_geocoder(anAdd, key=geocoder_dict[aGeocoder])
+                addr, lat, lng = getAddrAndLatLng(g)
+            result_dict[aGeocoder][base_address].append(addr)
+pickle.dump(result_dict, open(r"G:\PUBLIC\GIS\Geocoding\geocoder_comparison\py\pickle\full_test_suite_results.p", "wb"))
--- py/geocoder_comp_2017.py
+++ py/geocoder_comp_2017.py
@@ -4,7 +4,11 @@ Script to compare performance of various geocoders.
 Madeline Steele, TriMet
 """
 
-import geocoder, psycopg2, requests, json, time
+import geocoder
+import psycopg2
+import requests
+import json
+import time
 from ott.geocoder.geosolr import GeoSolr
 from unidecode import unidecode
 
@@ -46,7 +50,7 @@ def getAddrAndLatLng(g):
     return address, latitude, longitude
     
 
-def copyResultsToTable(anAddr, aLat, aLng):
+def copyResultsToTable(anAddr, aLat, aLng, aGeocoder, row, cur):
     """
     this function copies address string results and the distance in feet
     from "true location" to the table it converts lat lng numeric values
@@ -63,6 +67,7 @@ def copyResultsToTable(anAddr, aLat, aLng):
                     + anAddr + "', " + aGeocoder \
                     + "_dist_ft='NaN' WHERE id=" + str(row[4]) + ";"
     cur.execute(updateSQL)
+    return
 
 
 def parse_mapzen_response(txt):
@@ -144,101 +149,106 @@ def strip_addresses_of_zip_country(address_string):
             address_list_comma[len(address_list_comma) - 1].replace(zip_candidate, "").replace(" ", "")
     return ", ".join(a for a in address_list_comma)
 
-# some geocoders need api keys
-with open(geocoder_file, 'r') as inf:
-    geocoder_dict = eval(inf.read())
 
-db_creds = open(db_cred_file, 'r').read().strip()
+def geocode_addresses():
+    # some geocoders need api keys
+    with open(geocoder_file, 'r') as inf:
+        geocoder_dict = eval(inf.read())
 
-conn = psycopg2.connect(db_creds)
-cur = conn.cursor()
+    db_creds = open(db_cred_file, 'r').read().strip()
 
-# Trimet_adds is a table of addresses with lat lngs "verified" by foursquare
-# check-ins provided by Ervin Ruci in June 2016. A few rows have 0,0 for the
-# latlng - we are dropping these
+    conn = psycopg2.connect(db_creds)
+    cur = conn.cursor()
 
-# Careful with this line!
-cur.execute('DROP TABLE IF EXISTS geocoder_test')
+    # Trimet_adds is a table of addresses with lat lngs "verified" by foursquare
+    # check-ins provided by Ervin Ruci in June 2016. A few rows have 0,0 for the
+    # latlng - we are dropping these
 
-cur.execute('''CREATE TABLE geocoder_test AS SELECT *
-            FROM trimet_adds WHERE lat != 0
-            LIMIT 10;''') # using LIMIT for testing
+    # Careful with this line!
+    cur.execute('DROP TABLE IF EXISTS geocoder_test')
 
-cur.execute('''ALTER TABLE geocoder_test
-            ADD COLUMN id SERIAL NOT NULL PRIMARY KEY;''')
+    cur.execute('''CREATE TABLE geocoder_test AS SELECT *
+                FROM trimet_adds WHERE lat != 0
+                LIMIT 10;''') # using LIMIT for testing
 
-# adding blank results fields for each geocoder
-for aGeocoder in geocoder_dict.keys():
-    sqlString = "ALTER TABLE geocoder_test ADD " + aGeocoder \
-    + "_address text, ADD " + aGeocoder + "_dist_ft numeric;"
-    cur.execute(sqlString)
+    cur.execute('''ALTER TABLE geocoder_test
+                ADD COLUMN id SERIAL NOT NULL PRIMARY KEY;''')
 
-counter = 0
-#cur.execute("SELECT * FROM geocoder_test")
-cur.execute("SELECT * FROM geocoder_test WHERE google_dist_ft IS NULL;")
-for row in cur.fetchall():
-    g = ''
-    counter += 1
-    if counter % 50 == 0:
-        print counter, 'rows done'
-    if counter == 6:
-        break
-    
-    anAdd = strip_addresses_of_zip_country(row[0])
-    print '\n\n', anAdd, '\n'
+    # adding blank results fields for each geocoder
     for aGeocoder in geocoder_dict.keys():
-        if aGeocoder == 'trimet':
-            gc = tm_geocoder.query(anAdd, rows=1, start=0)
-            try:
-                results = gc.results[0]
-                addr = (str.decode('{0}, {1}, OR'.format(results['name'].replace("'", "\''"), results['city'])))
-            except:
-                addr = 'Null'
-                lat = 'Null'
-                lng = 'Null'
-            print aGeocoder, ':', addr, ", maxScore:", gc.maxScore
-        # I have to call mapzen outside of the geocoder wrapper because
-        # without the bbox parameters (which I can't pass through geocoder)
-        # it was returning some very wacky results - Kansas, Iowa, FL -
-        # and just in the first 10 records.
-        elif aGeocoder == 'mapzen':
-            request = "https://search.mapzen.com/v1/search?api_key=" \
-                    + geocoder_dict[aGeocoder] + "&text=" + anAdd + bbox
-            resp = requests.get(request, verify=False).text
-            result = parse_mapzen_response(resp)
-            addr = result['label']
-            lat = result['latitude']
-            lng = result['longitude']
-            print aGeocoder, ':', addr, ", confidence:", result['confidence']
-        elif aGeocoder == 'rlis':
-            result = rlis_geocode(anAdd, geocoder_dict[aGeocoder])
-            if result:
-                addr = result['fullAddress']
-                lat = result['lat']
-                lng = result['lng']
-                print aGeocoder, addr, 'score:', result['score']
-            else:
-                addr = 'Null'
-                lat = 'Null'
-                lng = 'Null'
-        else:
-            this_geocoder = getattr(geocoder, aGeocoder)
-            if geocoder_dict[aGeocoder] == '':
-                g = this_geocoder(anAdd)
-                while g.status == 'OVER_QUERY_LIMIT':
-                    print ("over", aGeocoder,
-                           "query limit - will try again in 30 minutes")
-                    time.sleep(1800)
-                    g = this_geocoder(anAdd)
-            else:
-                # another possible parameter to pass for mapbox is
-                # proximity=pdxlatlng (makes it favor results near that
-                # point) but it didn't improve things for a small sample 
-                g = this_geocoder(anAdd, key=geocoder_dict[aGeocoder]) 
-            
-            print aGeocoder, ':', g.address, ", conf.:", g.confidence, ", qual.:", g.quality
+        sqlString = "ALTER TABLE geocoder_test ADD " + aGeocoder \
+        + "_address text, ADD " + aGeocoder + "_dist_ft numeric;"
+        cur.execute(sqlString)
 
-            addr, lat, lng = getAddrAndLatLng(g)
-        copyResultsToTable(addr, lat, lng)
-    conn.commit()    
-conn.close()
+    counter = 0
+    #cur.execute("SELECT * FROM geocoder_test")
+    cur.execute("SELECT * FROM geocoder_test WHERE google_dist_ft IS NULL;")
+    for row in cur.fetchall():
+        g = ''
+        counter += 1
+        if counter % 50 == 0:
+            print counter, 'rows done'
+        if counter == 6:
+            break
+
+        anAdd = strip_addresses_of_zip_country(row[0])
+        print '\n\n', anAdd, '\n'
+        for aGeocoder in geocoder_dict.keys():
+            if aGeocoder == 'trimet':
+                gc = tm_geocoder.query(anAdd, rows=1, start=0)
+                try:
+                    results = gc.results[0]
+                    addr = (str.decode('{0}, {1}, OR'.format(results['name'].replace("'", "\''"), results['city'])))
+                except:
+                    addr = 'Null'
+                    lat = 'Null'
+                    lng = 'Null'
+                print aGeocoder, ':', addr, ", maxScore:", gc.maxScore
+            # I have to call mapzen outside of the geocoder wrapper because
+            # without the bbox parameters (which I can't pass through geocoder)
+            # it was returning some very wacky results - Kansas, Iowa, FL -
+            # and just in the first 10 records.
+            elif aGeocoder == 'mapzen':
+                request = "https://search.mapzen.com/v1/search?api_key=" \
+                        + geocoder_dict[aGeocoder] + "&text=" + anAdd + bbox
+                resp = requests.get(request, verify=False).text
+                result = parse_mapzen_response(resp)
+                addr = result['label']
+                lat = result['latitude']
+                lng = result['longitude']
+                print aGeocoder, ':', addr, ", confidence:", result['confidence']
+            elif aGeocoder == 'rlis':
+                result = rlis_geocode(anAdd, geocoder_dict[aGeocoder])
+                if result:
+                    addr = result['fullAddress']
+                    lat = result['lat']
+                    lng = result['lng']
+                    print aGeocoder, addr, 'score:', result['score']
+                else:
+                    addr = 'Null'
+                    lat = 'Null'
+                    lng = 'Null'
+            else:
+                this_geocoder = getattr(geocoder, aGeocoder)
+                if geocoder_dict[aGeocoder] == '':
+                    g = this_geocoder(anAdd)
+                    while g.status == 'OVER_QUERY_LIMIT':
+                        print ("over", aGeocoder,
+                               "query limit - will try again in 30 minutes")
+                        time.sleep(1800)
+                        g = this_geocoder(anAdd)
+                else:
+                    # another possible parameter to pass for mapbox is
+                    # proximity=pdxlatlng (makes it favor results near that
+                    # point) but it didn't improve things for a small sample
+                    g = this_geocoder(anAdd, key=geocoder_dict[aGeocoder])
+
+                print aGeocoder, ':', g.address, ", conf.:", g.confidence, ", qual.:", g.quality
+
+                addr, lat, lng = getAddrAndLatLng(g)
+            copyResultsToTable(addr, lat, lng, aGeocoder, row, cur)
+        conn.commit()
+    conn.close()
+    return
+
+# geocode_addresses()
